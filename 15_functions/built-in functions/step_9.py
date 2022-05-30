0eb52d6a8ce45f9cada72961cbcbc064c0bd5ad5
Viacheslav <yershovviacheslav@gmail.com>
2022-05-30 15:56:54 +0300

    15.8_built-in_functions solved step8, step_9

---
+++ 15_functions/built-in functions/step_9.py
@@ -0,0 +1,9 @@
+"""Используя параллельную итерацию сразу по трем спискам countries, capitals и population выведите информацию о стране в формате:
+<capital> is the capital of <country>, population equal <population> people."""
+
+countries = ['Russia', 'USA', 'UK', 'Germany', 'France', 'India']
+capitals = ['Moscow', 'Washington', 'London', 'Berlin', 'Paris', 'Delhi']
+population = [145_934_462, 331_002_651, 80_345_321, 67_886_011, 65_273_511, 1_380_004_385]
+
+for capital, country, people in zip(capitals, countries, population):
+    print(f'{capital} is the capital of {country}, population equal {people} people.')
